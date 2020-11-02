471e82fc33692161af329b7797fb1263c79c599b
Andrew Eremenko <angennere@gmail.com>
2020-11-02 07:54:33 +0300

    some implementations of localizedControllerException was benn added

--- module2/web/src/main/java/com/epam/esm/exception/ExceptionDetail.java
+++ module2/web/src/main/java/com/epam/esm/exception/ExceptionDetail.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionDetail {
 
     TAG_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "exception.message.40401", 40401),
-    TAG_DOES_NOT_CONTAIN_NAME(HttpStatus.BAD_REQUEST, "exception.message.40001", 40001);
+    TAG_DOES_NOT_CONTAIN_NAME(HttpStatus.BAD_REQUEST, "exception.message.40001", 40001),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "exception.message.50001", 50001),
+    UPDATING_FORBIDDEN_DATE_FIELDS(HttpStatus.FORBIDDEN, "exception.message.40301",40301);
 
     @Getter private final HttpStatus httpStatus;
     @Getter private final String message;
