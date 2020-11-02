706d5479a2c78069285da72666b63882f9d6d96e
Andrew Eremenko <angennere@gmail.com>
2020-11-02 07:49:11 +0300

    enebled transaction management

--- module2/service/src/main/java/com/epam/esm/config/ServiceConfig.java
+++ module2/service/src/main/java/com/epam/esm/config/ServiceConfig.java
@@ -5,10 +5,11 @@ import com.epam.esm.service.TagServiceImpl;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @Import(JdbcConfig.class)
 @ComponentScan(basePackageClasses = {TagServiceImpl.class, GiftCertificateServiceImpl.class})
+@EnableTransactionManagement
 public class ServiceConfig {
-
 }
