7775d6dfbe82df4ea31234410dc010598e805732
Andrew Eremenko <angennere@gmail.com>
2020-11-06 06:54:30 +0300

    added exception handler handles unregister runtime exceptions

--- module2/web/src/main/java/com/epam/esm/controller/GlobalExceptionControllerAdviser.java
+++ module2/web/src/main/java/com/epam/esm/controller/GlobalExceptionControllerAdviser.java
@@ -1,8 +1,13 @@
 package com.epam.esm.controller;
 
+import com.epam.esm.exception.ExceptionDetail;
 import com.epam.esm.exception.LocalizedControllerException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,9 +17,10 @@ import java.util.Locale;
 import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionController {
-    @Autowired
-    private ResourceBundleMessageSource messageSource;
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class GlobalExceptionControllerAdviser {
+    ResourceBundleMessageSource messageSource;
 
     @ExceptionHandler({LocalizedControllerException.class})
     public ResponseEntity<Object> handleException(LocalizedControllerException ex, Locale locale) {
@@ -25,6 +31,18 @@ public class GlobalExceptionController {
     }
 
     private String resolveResourceBundle(Exception ex, Locale locale) {
-        return messageSource.getMessage(ex.getMessage(), new Object[]{null}, locale);
+        return messageSource.getMessage(ex.getMessage(), null, locale);
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleUnregisterException(Locale locale){
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("errorMessage", resolveResourceBundle(locale));
+        parameters.put("errorCode", 50001);
+        return new ResponseEntity<>(parameters, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private String resolveResourceBundle(Locale locale) {
+        return messageSource.getMessage(ExceptionDetail.INTERNAL_SERVER_ERROR.getMessage(), null, locale);
     }
 }
