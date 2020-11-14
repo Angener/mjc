e383e70ed85a4269e755d6d979778e5c67067f06
Andrew Eremenko <angennere@gmail.com>
2020-11-14 15:44:52 +0300

    included duolicatedKeyException to tagController

--- module2/web/src/main/java/com/epam/esm/controller/TagController.java
+++ module2/web/src/main/java/com/epam/esm/controller/TagController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -47,9 +48,11 @@ public class TagController {
 
     @PostMapping("/tags")
     @ResponseStatus(HttpStatus.CREATED)
-    public long save(@RequestBody Tag tag) {
+    public Tag save(@RequestBody Tag tag) {
         try {
             return service.save(tag);
+        } catch (DuplicateKeyException ex){
+            throw new LocalizedControllerException(ExceptionDetail.NAME_IS_NOT_UNIQUE);
         } catch (DataIntegrityViolationException ex) {
             throw new LocalizedControllerException(ExceptionDetail.TAG_DOES_NOT_CONTAIN_NAME);
         }
