77aee39d41e92579af17ee96d7a6ca1a18c6f659
Andrew Eremenko <angennere@gmail.com>
2020-11-15 01:34:46 +0300

    add all args constractor

--- module2/service/src/main/java/com/epam/esm/dto/GiftCertificateDto.java
+++ module2/service/src/main/java/com/epam/esm/dto/GiftCertificateDto.java
@@ -2,6 +2,7 @@ package com.epam.esm.dto;
 
 import com.epam.esm.entity.GiftCertificate;
 import com.epam.esm.entity.Tag;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.Set;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class GiftCertificateDto {
     private GiftCertificate giftCertificate;
     private Set<Tag> tags;
